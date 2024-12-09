9f56d713ffba8b65a998b820c0e32e097412a259
jhqiu21 <142405039+jhqiu21@users.noreply.github.com>
2024-12-09 18:57:33 +0800

    Add comment

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import "./styles.css";
 
 import App from "./App";
 
+/**
+ * Brings all the pieces together and injects the final product into index.html in the public folder
+ */
+
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
